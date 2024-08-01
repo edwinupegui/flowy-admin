c2e8614bc0577152f67bc71fe630f5aa308287f6
Edwin Upegui <edwinupegui@gmail.com>
2024-08-01 09:09:06 -0500

    logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
-import { Pagination, Tab, Tabs } from '@nextui-org/react'
+import { Button, Pagination, Tab, Tabs } from '@nextui-org/react'
 
 import DocumentManagerTable from '@/components/organisms/DocumentManagerTable'
 
@@ -26,50 +26,67 @@ export default function HomePage() {
     }
   }, [])
 
+  const handleLogout = () => {
+    localStorage.removeItem('user')
+    router.push('/login')
+  }
+
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="flex flex-col items-start">
-        <h1 className="text-2xl font-bold">Gestor de documentos</h1>
-        <div className="mt-5 flex gap-4">
-          <div className="flex flex-col">
-            <Tabs color="primary" radius="full" size="md">
-              <Tab title={`Todos (${users.length})`}>
-                <div className="mt-2">
-                  <DocumentManagerTable
-                    rows={users}
-                    selectedKeys={selectedKeys}
-                    setSelectedKeys={setSelectedKeys}
-                    isLoading={isLoading}
-                  />
-                </div>
-              </Tab>
-              <Tab title={`Aprobados (${users.length})`}>
-                <div className="mt-2">
-                  <DocumentManagerTable
-                    rows={users}
-                    selectedKeys={selectedKeys}
-                    setSelectedKeys={setSelectedKeys}
-                    isLoading={isLoading}
-                  />
-                </div>
-              </Tab>
-              <Tab title={`Denegados (${users.length})`}>
-                <div className="mt-2">
-                  <DocumentManagerTable
-                    rows={users}
-                    selectedKeys={selectedKeys}
-                    setSelectedKeys={setSelectedKeys}
-                    isLoading={isLoading}
-                  />
-                </div>
-              </Tab>
-            </Tabs>
+    <>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="flex flex-col items-start">
+          <div className="flex w-full justify-end">
+            <Button
+              variant="solid"
+              color="primary"
+              radius="full"
+              onClick={handleLogout}
+            >
+              Salir
+            </Button>
+          </div>
+          <h1 className="text-2xl font-bold">Gestor de documentos</h1>
+          <div className="mt-5 flex gap-4">
+            <div className="flex flex-col">
+              <Tabs color="primary" radius="full" size="md">
+                <Tab title={`Todos (${users.length})`}>
+                  <div className="mt-2">
+                    <DocumentManagerTable
+                      rows={users}
+                      selectedKeys={selectedKeys}
+                      setSelectedKeys={setSelectedKeys}
+                      isLoading={isLoading}
+                    />
+                  </div>
+                </Tab>
+                <Tab title={`Aprobados (${users.length})`}>
+                  <div className="mt-2">
+                    <DocumentManagerTable
+                      rows={users}
+                      selectedKeys={selectedKeys}
+                      setSelectedKeys={setSelectedKeys}
+                      isLoading={isLoading}
+                    />
+                  </div>
+                </Tab>
+                <Tab title={`Denegados (${users.length})`}>
+                  <div className="mt-2">
+                    <DocumentManagerTable
+                      rows={users}
+                      selectedKeys={selectedKeys}
+                      setSelectedKeys={setSelectedKeys}
+                      isLoading={isLoading}
+                    />
+                  </div>
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
+          <div className="flex w-full justify-end">
+            <Pagination isCompact showControls total={10} initialPage={1} />
           </div>
         </div>
-        <div className="flex w-full justify-end">
-          <Pagination isCompact showControls total={10} initialPage={1} />
-        </div>
       </div>
-    </div>
+    </>
   )
 }
